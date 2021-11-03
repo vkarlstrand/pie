03edf94ee46a80150bf62fd3ec0af92048003d71
vkarlstrand <viktor.karlstrand@gmail.com>
2021-11-03 17:28:29 +0100

    Normalization, training, load trained model, and test

--- data.py
+++ data.py
@@ -2,40 +2,166 @@
 import os
 import numpy as np
 import pandas as pd
+import torch
 from torch.utils.data import Dataset, DataLoader
-from PIL import Image
+import PIL
 import cv2
 
 
 
-def default_loader(path):
-    return Image.open(path).convert('RGB')
+def partition_data(file='data/data_labels.csv', ratios=[0.70, 0.20, 0.10], out_path='data/'):
+    """
+    Partition data into training, validation and test data. Saves partitioned data
+    to three .csv files with equally many samples from each class in each file.
+    """
+
+    # Define ratios for traning, validation and test
+    ratio_train = ratios[0]
+    ratio_validation = ratios[1]
+    ratio_test = ratios[2]
+
+    # Import data
+    data = np.genfromtxt(file, skip_header=1, dtype='str', delimiter=',')
+
+    # Divide by image and label
+    imgs = data[:,0]
+    lbls = data[:,1]
+
+    # Find indices of each class
+    idxs0 = np.where(lbls=='0')
+    idxs1 = np.where(lbls=='1')
+    idxs2 = np.where(lbls=='2')
+
+    # Find number of samples for each class
+    N0 = len(idxs0[0])
+    N1 = len(idxs1[0])
+    N2 = len(idxs2[0])
+    N = min(N0, N1, N2)
+
+    # Divide data by classes
+    imgs0 = imgs[idxs0]
+    lbls0 = lbls[idxs0]
+    imgs1 = imgs[idxs1]
+    lbls1 = lbls[idxs1]
+    imgs2 = imgs[idxs2]
+    lbls2 = lbls[idxs2]
+
+    # Shuffle order
+    idxs_shuffle0 = np.random.permutation(N0)
+    idxs_shuffle1 = np.random.permutation(N1)
+    idxs_shuffle2 = np.random.permutation(N2)
+    imgs0 = imgs0[idxs_shuffle0]
+    lbls0 = lbls0[idxs_shuffle0]
+    imgs1 = imgs1[idxs_shuffle1]
+    lbls1 = lbls1[idxs_shuffle1]
+    imgs2 = imgs2[idxs_shuffle2]
+    lbls2 = lbls2[idxs_shuffle2]
+
+    # Take samples from each class
+    imgs = []
+    lbls = []
+    for n in range(N):
+        imgs.append(imgs0[n])
+        lbls.append(lbls0[n])
+        imgs.append(imgs1[n])
+        lbls.append(lbls1[n])
+        imgs.append(imgs2[n])
+        lbls.append(lbls2[n])
+
+    # Divide into training, validation and test data sets
+    idxs_train = int(ratio_train*len(imgs))
+    idxs_validation = int((ratio_train+ratio_validation)*len(imgs))
+    imgs_train = imgs[0:idxs_train]
+    imgs_validation = imgs[idxs_train:idxs_validation]
+    imgs_test = imgs[idxs_validation:]
+    lbls_train = lbls[0:idxs_train]
+    lbls_validation = lbls[idxs_train:idxs_validation]
+    lbls_test = lbls[idxs_validation:]
+
+    # Save to files
+    np.savetxt(out_path+'data_labels_train.csv', np.vstack((imgs_train, lbls_train)).T, delimiter=',', fmt='%s')
+    np.savetxt(out_path+'data_labels_validation.csv', np.vstack((imgs_validation, lbls_validation)).T, delimiter=',', fmt='%s')
+    np.savetxt(out_path+'data_labels_test.csv', np.vstack((imgs_test, lbls_test)).T, delimiter=',', fmt='%s')
+
+    # Print information
+    print('PARTITIONED DATA')
+    print('  Training:   ' + str(int(100*ratios[0]))+'%, ' + str(len(lbls_train)) + ' samples')
+    print('  Validation: ' + str(int(100*ratios[1]))+'%, ' + str(len(lbls_validation)) + ' samples')
+    print('  Testing:    ' + str(int(100*ratios[2]))+'%, ' + str(len(lbls_test)) + ' samples')
+
+
+
+def default_image_loader(image_path):
+    """
+    Helper function to open image file as PIL image in RBG format.
+    """
+    return PIL.Image.open(image_path).convert('RGB')
+
+
 
 class DatasetFromCSV(Dataset):
-    def __init__(self, image_root, csv_path, transforms=None, loader=default_loader):
-
+    """
+    Class to import dataset from .csv files. Can perform transformations defined
+    by agumentations using albumentations.
+    The class inhereits from from torch.utils.data's Dataset.
+    """
+    def __init__(self, image_root, csv_path, transforms=None, loader=default_image_loader):
+        # Root directory containing images
         self.image_root = image_root
-
+        # Read data using pandas
         self.data = pd.read_csv(csv_path, header=None)
-        self.labels = np.asarray(self.data.iloc[:, 1])
-
+        # Initialize list for images
         imgs = []
-        files_names = np.array(self.data.iloc[:, 0])
+        # Get file names in first column as numpy array
+        files_names = np.array(self.data.iloc[:,0])
+        # Iterate over all files names and join the image name with the root path
         for img in files_names:
             imgs.append(os.path.join(self.image_root, str(img)))
-
+        # Get labels in second column as numpy array
+        self.labels = np.asarray(self.data.iloc[:,1])
+        # Define images, now with full path, as attribute
         self.images = imgs
+        # Define provided transform as attribute
         self.transforms = transforms
+        # Use the provided loader to load the images
         self.loader = loader
 
     def __getitem__(self, index):
-
-        label = self.labels[index]
+        # Get image and label
         img = self.images[index]
+        lbl = self.labels[index]
+        # Load image using the loader
         img = np.array(self.loader(img))
+        # Perform transormation if it was provided
         if self.transforms is not None:
             img = self.transforms(image=img)
-        return img, label
+        return img, lbl
 
     def __len__(self):
+        # Return number of samples in dataset
         return len(self.data.index)
+
+
+
+def compute_mean_std(dataset):
+    """
+    Compute mean and standard deviation of dataset of images.
+    """
+    # Get image size
+    image_size = dataset[0][0]['image'].shape[1]
+    # Load data
+    dataloader = DataLoader(dataset, batch_size=32)
+    # Initialize sums for results
+    total_sum = torch.tensor([0.0, 0.0, 0.0])
+    total_sum_square = torch.tensor([0.0, 0.0, 0.0])
+    # Iterate over all data and sum sums of batches
+    for data in dataloader:
+        img = data[0]['image']
+        total_sum += img.sum(axis=[0,2,3])
+        total_sum_square += (img**2).sum(axis=[0,2,3])
+    # Compute total pixel count and overall mean and std
+    total_pixel_count = len(dataset) * image_size * image_size
+    mean = total_sum/total_pixel_count
+    std = torch.sqrt((total_sum_square/total_pixel_count)-(mean**2))
+    # Return results
+    return mean, std
--- plot.py
+++ plot.py
@@ -1,12 +1,33 @@
+import numpy as np
 import matplotlib.pyplot as plt
+import albumentations as album
+import torch
+import torchvision
 
-def plot_dataset(dataset, rows, cols, image_width):
+
+
+def plot_dataset(dataset, rows, cols, image_width, mean, std):
+    """
+    Plot some examples from dataset by first unnormalize with provided
+    mean and std to a range in approximately [0,1].
+    """
+    # Initialize figure and axes
     fig, axs = plt.subplots(rows, cols, figsize=(cols*image_width, rows*image_width))
+    # Iterate over rows and columns
     for row in range(rows):
         for col in range(cols):
+            # Extract image and label
             img = dataset[row*cols+col][0]['image']
             lbl = dataset[row*cols+col][1]
-            axs[row,col].imshow(img.permute(1,2,0))
+            # Change order of dimensions and convert to numpy array
+            img = img.permute(1,2,0).numpy()
+            # Unnormalize the image to range [0,1] so that it can be plotted with imshow
+            mean_inv = torch.div(-mean, std)
+            std_inv = torch.div(1, std)
+            album_inv = album.Compose([album.Normalize(mean=mean_inv, std=std_inv, max_pixel_value=1.0)])
+            img = album_inv(image=img)['image']
+            # Plot image
+            axs[row,col].imshow(img)
             axs[row,col].axis('off')
             axs[row,col].set_title('Class '+str(lbl), fontsize=20)
     return fig, axs
